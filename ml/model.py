9319d542c2cd7026408af77153b29979d4fb388d
jlsawchak <jlsawchak@outlook.com>
2024-12-13 23:53:35 -0500

    implements steps 2 through 4

--- ml/model.py
+++ ml/model.py
@@ -1,7 +1,7 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
-# TODO: add necessary import
+from sklearn.ensemble import RandomForestClassifier
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -20,7 +20,11 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
     # TODO: implement the function
-    pass
+    model = RandomForestClassifier(random_state=42)
+
+    model.fit(X_train, y_train)
+
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -60,7 +64,9 @@ def inference(model, X):
         Predictions from the model.
     """
     # TODO: implement the function
-    pass
+    preds = model.predict(X)
+
+    return preds
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -72,13 +78,25 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
-    pass
+    try:
+        with open(path, 'wb') as f:
+            pickle.dump(model, f)
+        print(f"Model saved to {path}")
+
+    except Exception as e:
+        print(f"Error saving {e}")
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
-    pass
+    try:
+        with open(path, 'rb') as f:
+            model = pickle.load(f)
+        print(f"Model loaded from {path}")
+
+        return model
+
+    except Exception as e:
+        print(f"Error loading {e}")
 
 
 def performance_on_categorical_slice(
@@ -119,10 +137,14 @@ def performance_on_categorical_slice(
     """
     # TODO: implement the function
     X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
-        # use training = False
+        data,
+        categorical_features=categorical_features,
+        label=label,
+        encoder=encoder,
+        lb=lb,
+        training=False
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
+
     return precision, recall, fbeta
